658e3b01a0dc3d65d5279e649f66118b3f54384f
Ivan Ogloblin <vip.ogloblin@bk.ru>
2023-10-23 17:31:36 +0800

    first commit

---
+++ SecondLesson/tests/FirstContract.spec.ts
@@ -0,0 +1,40 @@
+import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { toNano } from 'ton-core';
+import { FirstContract } from '../wrappers/FirstContract';
+import '@ton-community/test-utils';
+
+describe('FirstContract', () => {
+    let blockchain: Blockchain;
+    let firstContract: SandboxContract<FirstContract>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        firstContract = blockchain.openContract(await FirstContract.fromInit());
+
+        const deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await firstContract.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: firstContract.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and firstContract are ready to use
+    });
+});
